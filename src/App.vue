9cd72d84d3acf7494448d7636836683b2e9566b2
Imp-boy <1126434179@qq.com>
2019-06-22 15:00:33 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <login loginName="Jonh"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import login from './components/login.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    login
   }
 }
 </script>
---
+++ src/components/login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>
+      <label>用户名</label>
+      <input v-model="loginName"/>
+    </div>
+    <div>
+      <label>密码</label>
+      <input />
+    </div>
+    <div>
+      <label>用户名</label>
+      <input />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    name:"login",
+    props:{
+        loginName:String
+    }
+}
+</script>
